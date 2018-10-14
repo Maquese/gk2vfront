55ea61a657a195f66c809d4dea307c4b8197ee0d
gbuccir <gbucci@uol.com.br>
2018-10-14 16:57:18 -0300

    mudança de nome do arquivo para nao dar conflito

--- gk2vFront/wwwroot/js/appGet.js
+++ gk2vFront/wwwroot/js/appGet.js
@@ -1,5 +1,5 @@
-var app = angular.module("app",[]);
-app.controller("appController",function($scope,$http)
+var app = angular.module("appGet",[]);
+app.controller("appControllerGet",function($scope,$http)
 {
     /// teste de chamanda funcionando 100%
     $http.get("http://localhost:7000/api/Match/ListarPartidas",{})
