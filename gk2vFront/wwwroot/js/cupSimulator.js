b86abb15b49b34888b7b8199050d01458c6fff92
Your Name <kenneymaquese@gmail.com>
2018-10-17 13:54:40 -0300

    parte do front controle

---
+++ gk2vFront/wwwroot/js/cupSimulator.js
@@ -0,0 +1,18 @@
+angular.module("app") /// seguindo assim pode ser sem modulos novos só pedir sempre o modulo de app fodasse a performance :)
+  .component("cupSimulator",{ //// nomo do componente no html trasformar as maiusculas em traço mais a letra maiscula em minuscula exemplo view-teste
+      templateUrl: '../html/cupsimulator.html', ///caminho do seu html brown
+      bindings: { name: '@' }, /// se precisar binda pra passar parametros para seus componentes mas recomendo usar uma serivice
+      controller: function(){ /// chamada ao iniciar seu componente
+        var $ctrl = this;
+        $ctrl.chamaPrimo = chamaPrimo;
+
+        function chamaPrimo(){
+            console.log("chamou o primo no log");
+        };
+        
+        $ctrl.nome = "cu";
+
+
+        //// aqui as logicas da tela/regras da tela criando functions e suas properts lembra de usar o $ctrl na view par apontar
+      }
+  });
--- gk2vFront/wwwroot/js/login.js
+++ gk2vFront/wwwroot/js/login.js
@@ -1,14 +1,26 @@
-var app = angular.module("loginApp", []);
+var app = angular.module("app");
 
-app.controller("loginCtrl", function ($scope, $http) {
+app.component("loginApp",{ //// nomo do componente no html trasformar as maiusculas em traço mais a letra maiscula em minuscula exemplo view-teste
+      templateUrl: '../html/login.html', ///caminho do seu html brown
+      bindings: { name: '@' }, /// se precisar binda pra passar parametros para seus componentes mas recomendo usar uma serivice
+      controller: function($rootScope){ /// chamada ao iniciar seu componente
+        var $ctrl = this;
+        
+        $ctrl.chamaPrimo = chamaPrimo;
 
-    $scope.user = {};
+        function chamaPrimo(){
+            console.log("chamou o primo no log");
+        };
+        
+        $ctrl.nome = "cu";
+
+        $ctrl.user = {};
 
     var api = 'api/user/CriarUsuario';
 
-    $scope.salvarUsuario = function (user) {
+    $ctrl.salvarUsuario = function (user) {
         if (user.senha == user.senhaConfirma && user.policy)
-            $http.post(api, $scope.user)
+            $http.post(api, $ctrl.user)
             .success(function (response) {
                 alert("Usuario salvo com sucesso!");
             }).error(function (error) {
@@ -19,15 +31,18 @@ app.controller("loginCtrl", function ($scope, $http) {
     };
 
 
-    $scope.efetuarLogin = function () {
-        console.log($scope.user);
-        $http.post(api, $scope.user)
+    $ctrl.efetuarLogin = function () {
+        console.log($ctrl.user);
+        $http.post(api, $ctrl.user)
             .success(function (response) {
                 alert("Usuario salvo com sucesso!");
             }).error(function (error) {
                 alert("Falha ao salvar o usuário");
             })
-    }
+    } 
 
-
-});
+    $rootScope.logado = false;
+    console.log("chamou");
+       }       //// aqui as logicas da tela/regras da tela criando functions e suas properts lembra de usar o $ctrl na view par apontar
+      
+  });
