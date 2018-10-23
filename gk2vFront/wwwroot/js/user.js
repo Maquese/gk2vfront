380164c31ea8ab86691862d0c6f9d55f418f9c11
gbuccir <gbucci@uol.com.br>
2018-10-22 22:58:58 -0300

    chamada server-side nao passando os parametros

--- gk2vFront/wwwroot/js/user.js
+++ gk2vFront/wwwroot/js/user.js
@@ -2,13 +2,14 @@ angular.module("app") /// seguindo assim pode ser sem modulos novos só pedir se
     .component("userApp", { //// nomo do componente no html trasformar as maiusculas em traço mais a letra maiscula em minuscula exemplo view-teste
         templateUrl: '../html/user.html', ///caminho do seu html brown
         bindings: { name: '@' }, /// se precisar binda pra passar parametros para seus componentes mas recomendo usar uma serivice
-        controller: function () { /// chamada ao iniciar seu componente
+        controller: function ($http) { /// chamada ao iniciar seu componente
             var $ctrl = this;
 
             $ctrl.user = {};
 
-            var api = 'api/user/CriarUsuario';
+            //var api = 'api/Usuario/Cadastrar';
 
+            var api = 'http://localhost:7000/api/Usuario/Cadastrar';
             $ctrl.salvarUsuario = function (user) {
                 if (user.nome == '' || user.nomeSistema == '' || user.senha == '' || user.senhaConfirma == '' || user.mail == '')
                     alert("Preencha os campos obrigatorios");
@@ -24,6 +25,24 @@ angular.module("app") /// seguindo assim pode ser sem modulos novos só pedir se
                         alert("Senhas diferentes");
             };
 
+            function preencherUser() {
+                var api = 'http://localhost:7000/api/Usuario/Login';
+                $ctrl.user.mail = "g@gk2v.com";
+                $ctrl.user.senha = "g";
+                var params = { 
+                    email: $ctrl.user.mail,
+                    senha: $ctrl.user.senha
+                 }
+
+                $http.post(api, params)
+                    .success(function (response) {
+                        $ctrl.user = response.data;
+                    }).error(function (error) {
+                        alert("Falha ao buscar o usuário");
+                    })
+            }
+
+            preencherUser();
 
             $ctrl.cancelarUsuario = function () {
                 $ctrl.user = {};
