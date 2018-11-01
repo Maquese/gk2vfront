905e57561a17c55d28aafa0d8796e8d03f71c898
gbuccir <gbucci@uol.com.br>
2018-11-01 00:03:25 -0300

    quiz

--- gk2vFront/wwwroot/js/quiz.js
+++ gk2vFront/wwwroot/js/quiz.js
@@ -5,10 +5,48 @@ app.component("quizApp", { //// nomo do componente no html trasformar as maiuscu
     bindings: { name: '@' }, /// se precisar binda pra passar parametros para seus componentes mas recomendo usar uma serivice
     controller: function ($rootScope, $http) { /// chamada ao iniciar seu componente
         var $ctrl = this;
+        $("body").css("background", "");
 
-        $("body").css("background", "url('../images/bg-1.jpg') no-repeat center center fixed");
+        $ctrl.escolherResposta = escolherResposta;
+        $ctrl.responder = responder;
 
-        alert("quiz");
+        $ctrl.pergunta = {
+            enunciado: "Quantos gols o Pelé marcou em toda a Copa do Mundo?",
+            resp1: "53 gols",
+            resp2: "75 gols",
+            resp3: "35 gols",
+            resp4: "82 gols",
+            respostaCorreta: ""
+        }
+
+        $ctrl.respostaUser = "";
+
+        function escolherResposta(value) {
+            $ctrl.resposta = value;
+        }
+
+        function responder() {
+            if ($ctrl.respostaUser == $ctrl.pergunta.respostaCorreta) {
+                alert("Certa a resposta");
+            }
+            else {
+                alert("ERRRROOOOOUU");
+            }
+        }
+
+        function getQuiz() {
+            var api = "ResponderQuiz"
+            $http.post(api, { resposta: $ctrl.resposta })
+                .success(function (response) {
+                    $ctrl.pergunta = response;
+                }).error(function (error) {
+                    alert("Falha ao buscar quiz");
+                })
+        }
+
+        //getQuiz();
+
+
 
         //// aqui as logicas da tela/regras da tela criando functions e suas properts lembra de usar o $ctrl na view par apontar
     }
